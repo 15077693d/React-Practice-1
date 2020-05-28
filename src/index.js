9beeb908ed995b0fc1295686683ae59775201f1f
15077693d <15077693d@gmail.com>
2020-05-29 00:43:39 +0800

    try to understand state props component

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+class Square extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
+  
+  render() {
+    return (
+      <button className="square" onClick={()=>{
+        this.setState({value:"x"})
+      }}>
+        {this.state.value}
+      </button>
+    );
+  }
+  }
+  
+class Board extends React.Component {
+  renderSquare() {
+    return <Square/>;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+        <div className="board-row">
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+        <div className="board-row">
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
