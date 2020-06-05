3b1088382b6d3423674a1fb48b3ed2472a867af6
15077693d <15077693d@gmail.com>
2020-06-05 08:45:51 +0800

    change a bug from 律姐

--- src/components/game/game.component.jsx
+++ src/components/game/game.component.jsx
@@ -6,7 +6,7 @@ export class Game extends React.Component {
     super(props)
     this.state = {
       isX: true,
-      history:[{values:Array(9).fill(null),id:0}]
+      history: [{ values: Array(9).fill(null), id: 0 }]
     }
   }
 
@@ -22,55 +22,62 @@ export class Game extends React.Component {
       [0, 3, 6], [1, 4, 7],
       [2, 5, 8], [6, 4, 2]]
     let flag = false;
-    if (this.state.history.length>9){
+    if (this.state.history.length > 9) {
       flag = "No Win ..."
     }
-    
+
     combinations.forEach(combination => {
       let result = currentValues[combination[0]] + currentValues[combination[1]] + currentValues[combination[2]]
       if (result === "XXX" | result === "OOO") {
-        flag = "Winner "+result.slice(0,1)+" !!"
+        flag = "Winner " + result.slice(0, 1) + " !!"
       }
     });
-    
+
     return flag
   }
 
-  handleClickBoard = (i,time) => {
+  handleClickBoard = (i, time) => {
     const history = this.state.history.slice()
     const currentValues = this.state.history[time].values.slice()
     // no win, not fill full, is null
-    if (!this.isWin(currentValues) & history.length<10 & currentValues[i]===null) {
+    if (!this.isWin(currentValues) & history.length < 10 & currentValues[i] === null) {
       // plus new values to history
       let newValues = this.state.history[time].values.slice()
-      newValues[i] = this.state.isX?"X":"O"
+      newValues[i] = this.state.isX ? "X" : "O"
 
-      this.setState({ history: history.concat([{values:newValues,id:history.length}]) })
+      this.setState({ history: history.concat([{ values: newValues, id: history.length }]) })
       // change isX
       this.setState({
-        isX: this.state.isX? false:true
+        isX: this.state.isX ? false : true
       })
     }
   }
 
-  handleClickHistory = (time) =>{
-    this.setState({history:this.state.history.slice(0,time+1)})
+  handleClickHistory = (time) => {
+   
+    let flag = this.state.history[time].values.slice()
+    flag = flag.filter( value =>(value !==null)).length
+   
+    this.setState({
+      history: this.state.history.slice(0, time + 1),
+      isX: flag%2===0 ? true : false
+    }, console.log(this.state))
   }
 
   render() {
     let time = this.state.history.length - 1
     const currentValues = this.state.history[time].values.slice()
 
-    const status = this.isWin(currentValues) ? this.isWin(currentValues) : `Next player: ${this.state.isX?"X":"O"}`
-    
+    const status = this.isWin(currentValues) ? this.isWin(currentValues) : `Next player: ${this.state.isX ? "X" : "O"}`
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board time = {time}  handleClick = {this.handleClickBoard} values={currentValues}/>
+          <Board time={time} handleClick={this.handleClickBoard} values={currentValues} />
         </div>
         <div className="game-info">
           <div className="status">{status}</div>
-          <ol>{this.state.history.map((record)=>(<History handleClick={this.handleClickHistory} key={record.id} time = {record.id}/>))}</ol>
+          <ol>{this.state.history.map((record) => (<History handleClick={this.handleClickHistory} key={record.id} time={record.id} />))}</ol>
         </div>
       </div>
     );
